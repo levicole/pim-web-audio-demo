57b518603412849d8d102be58f5383bfe6c01f6a
Levi Kennedy <lckennedy@gmail.com>
2020-05-02 11:48:01 -0500

    Adds PPM

--- pim.js
+++ pim.js
@@ -1,13 +1,14 @@
-function init() {
-  const SAMPLE_RATE  = 220000;
+function init(ppm) {
+  const SAMPLE_RATE  = ppm ? 54000 : 220000;
   const TOTAL_VOICES = 4;
-  const DURATION     = 40;
-  const AMPLITUDE    = 1.0;
+  const DURATION     = 20;
+  const DC_OFFSET    = 0;
+  const AMPLITUDE    = 1;
   const TOTAL_SAMPLES = SAMPLE_RATE * DURATION;
 
-  let audioContext = new AudioContext()
+  window.audioContext = new AudioContext()
 
-  const audioBuffer   = audioContext.createBuffer(1, TOTAL_SAMPLES, SAMPLE_RATE);
+  window.audioBuffer   = audioContext.createBuffer(1, TOTAL_SAMPLES, SAMPLE_RATE);
   const buffer        = audioBuffer.getChannelData(0);
   const waveform      = new Float32Array(TOTAL_VOICES);
   const pitch_counter = new Float32Array(TOTAL_VOICES);
@@ -24,21 +25,43 @@ function init() {
   waveform[2]  = frequency[2] / 512; // 0.001% Duty Cycle
   waveform[3]  = frequency[3] / 512; // 0.001% Duty Cycle
 
-  let current_sample = 0
+  let current_sample = 0;
+  let aggregate_output = 0; // needed for PPM
 
   while(current_sample < TOTAL_SAMPLES) {
-    for(let voice = 0; voice < TOTAL_VOICES; ++voice) {
-      if (++pitch_counter[voice] === frequency[voice]) {
-        pitch_counter[voice] = 0.0;
-      } else if (pitch_counter[voice] <= waveform[voice]) {
-        buffer[++current_sample] = 1.0;
-      } else if (pitch_counter[voice] >= waveform[voice]) {
-        buffer[++current_sample] = 0.0;
-      }
+    if(ppm) {
+      // pin pulse method
+      for(let voice = 0; voice < TOTAL_VOICES; ++voice) {
+          if(++pitch_counter[voice] == frequency[voice]) {
+            pitch_counter[voice] = 0.0;
+          } else if(pitch_counter[voice] <= waveform[voice]) {
+            output[voice] = 1.0;
+          } else if(pitch_counter[voice] >= waveform[voice]) {
+            output[voice] = 0.0;
+          }
+          aggregate_output = output[voice] | aggregate_output;
 
-      if(current_sample % 25000 == 0) {
-        for(let voice = 0; voice < TOTAL_VOICES; ++voice) {
-          ++waveform[voice];
+          // increase duty cycle over time
+          if(current_sample % 12000 == 0)
+            ++waveform[voice];
+      }
+      buffer[++current_sample] = (aggregate_output * AMPLITUDE) + DC_OFFSET;
+      aggregate_output = 0;
+    } else {
+      // pulse interleaving modulation
+      for(let voice = 0; voice < TOTAL_VOICES; ++voice) {
+        if (++pitch_counter[voice] === frequency[voice]) {
+          pitch_counter[voice] = 0.0;
+        } else if (pitch_counter[voice] <= waveform[voice]) {
+          buffer[++current_sample] = DC_OFFSET + AMPLITUDE;
+        } else if (pitch_counter[voice] >= waveform[voice]) {
+          buffer[++current_sample] = DC_OFFSET;
+        }
+
+        if(current_sample % 25000 == 0) {
+          for(let voice = 0; voice < TOTAL_VOICES; ++voice) {
+            ++waveform[voice];
+          }
         }
       }
     }
